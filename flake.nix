3864cfc10e891cb8921e38e320534d6fde116851
Lord-Valen <lord_valen@protonmail.com>
2023-02-22 15:03:22 -0500

    :recycle: `env` Standardize

--- flake.nix
+++ flake.nix
@@ -1,53 +1,28 @@
 {
   description = "A DI Discord bot framework";
 
-  inputs.devshell = {
-    url = "github:numtide/devshell";
-    inputs = {
-      nixpkgs.follows = "nixpkgs";
-      flake-utils.follows = "flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    std = {
+      url = "github:divnix/std";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  # For rush, pending merge
-  inputs.rush-nixpkgs.url = "github:Lord-Valen/nixpkgs/init/nodePackages.rush";
-
   outputs = {
+    std,
     self,
-    nixpkgs,
-    flake-utils,
-    devshell,
-    rush-nixpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default = let
-        shellPath = builtins.path {
-          path = ./shell;
-          name = "shellPath";
-        };
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [devshell.overlay];
-        };
-        rushPkgs = rush-nixpkgs.legacyPackages.${system};
-      in
-        pkgs.devshell.mkShell {
-          imports = [
-            (pkgs.devshell.extraModulesDir + "/git/hooks.nix")
-            (pkgs.devshell.importTOML (shellPath + "/devshell.toml"))
-          ];
-
-          commands = [
-            {
-              category = "monorepo";
-              package = rushPkgs.nodePackages.rush;
-            }
-          ];
-
-          git.hooks = {
-            enable = true;
-            pre-commit.text = builtins.readFile (shellPath + "/pre-commit.sh");
-          };
-        };
-    });
+    ...
+  } @ inputs:
+    std.growOn {
+      inherit inputs;
+      cellsFrom = ./nix;
+      cellBlocks = with std.blockTypes; [
+        (devshells "devshells")
+        (nixago "configs")
+      ];
+    }
+    {
+      devShells = std.harvest self ["_automation" "devshells"];
+    };
 }
