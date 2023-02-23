885a8747ceff5d6a2b2b58f9d491e3b9e64beeb7
Lord-Valen <lord_valen@protonmail.com>
2023-02-23 17:23:13 -0500

    :bulb: `refactor` Use inherit

--- nix/_automation/utils.nix
+++ nix/_automation/utils.nix
@@ -5,5 +5,5 @@
   inherit (inputs) nixpkgs;
   lib = nixpkgs.lib // builtins;
 in {
-  mkD2nProjects = names: attrs: lib.genAttrs names (name: {name = name;} // attrs);
+  mkD2nProjects = names: attrs: lib.genAttrs names (name: {inherit name;} // attrs);
 }
