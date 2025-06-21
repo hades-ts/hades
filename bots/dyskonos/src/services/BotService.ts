0f1fa082289ba99f0c760ef6ecc40f9dba310936
Dustin Lacewell <dlacewell@gmail.com>
2025-06-21 14:59:35 -0500

    Have Dyskolos use SlashCommandBotService

--- bots/dyskonos/src/services/BotService.ts
+++ bots/dyskonos/src/services/BotService.ts
@@ -1,25 +1,6 @@
-import { inject } from "inversify";
-import { Interaction } from "discord.js";
 
-import { singleton, HadesBotService } from "@hades-ts/hades";
-import { SlashCommandService } from "@hades-ts/slash-commands";
+import { singleton } from "@hades-ts/hades";
+import { SlashCommandBotService } from "@hades-ts/slash-commands";
 
 @singleton(BotService)
-export class BotService extends HadesBotService {
-    @inject(SlashCommandService)
-    protected slashCommands: SlashCommandService;
-
-    async onReady() {
-        console.log(`Logged in as ${this.client.user.username}.`);
-        await this.slashCommands.registerCommands(this.client);
-        this.client.guilds.cache.forEach((guild) => {
-            console.log(` - ${guild.name} (${guild.id})`);
-        });
-    }
-
-    async onInteractionCreate<T extends Interaction>(interaction: T) {
-        if (interaction.isChatInputCommand()) {
-            this.slashCommands.dispatch(interaction);
-        }
-    }
-}
+export class BotService extends SlashCommandBotService {}
