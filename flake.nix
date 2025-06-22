7e09545d9b80af3e4842bf2fd034439e03be79a2
Lord-Valen <lord_valen@protonmail.com>
2025-06-22 17:40:18 -0400

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A DI Discord bot framework";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     devshell.url = "github:numtide/devshell";
     nixago.url = "github:nix-community/nixago";
