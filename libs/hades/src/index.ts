1f47d2edf45dd8409510952d9dd2e88e8a920e7b
Dustin Lacewell <dlacewell@gmail.com>
2025-06-21 20:04:05 -0500

    Add boot helper

--- libs/hades/src/index.ts
+++ libs/hades/src/index.ts
@@ -1,6 +1,29 @@
+import { interfaces } from "inversify";
+import { HadesBotService } from "./services";
+import { Installer } from "./Installer";
+import { HadesContainer } from "./HadesContainer";
+
 export * from "./decorators";
 export * from "./HadesContainer";
 export * from "./Installer";
 export * from "./interactions";
 export * from "./services";
 export * from "./utils";
+
+
+export const boot = async (
+    botService: interfaces.Newable<HadesBotService>,
+    installers?: Installer[]
+) => {
+    try {
+        const container = new HadesContainer({
+            installers: installers ?? [],
+        });
+
+        const bot = container.get(botService);
+        await bot.login();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
