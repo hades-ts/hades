44d48b963db7d875abb01561efca637bc0184b1f
Dustin Lacewell <dlacewell@gmail.com>
2025-07-02 03:59:01 -0500

    Break up LogViewer.tsx

---
+++ apps/log-viewer/src/components/FilterSection.tsx
@@ -0,0 +1,28 @@
+import { useFilterStore } from '../store/filterStore';
+import PropertyMultiSelect from './PropertyMultiSelect';
+import PropertyValueFilters from './PropertyValueFilters';
+import SearchBar from './SearchBar';
+import SpecialPropertyFilters from './SpecialPropertyFilters';
+
+export default function FilterSection() {
+    const { messageFilter, setMessageFilter } = useFilterStore();
+
+    return (
+        <div className="mb-6 space-y-6">
+            {/* 1. Message Search */}
+            <SearchBar
+                value={messageFilter}
+                onChange={setMessageFilter}
+            />
+
+            {/* 2. Property Selection Dropdown */}
+            <PropertyMultiSelect />
+
+            {/* 3. Permanent Value Filters (Special Properties) */}
+            <SpecialPropertyFilters />
+
+            {/* 4. Enabled Value Filters (Selected Properties) */}
+            <PropertyValueFilters />
+        </div>
+    );
+} 
